ae61aac6490aced2a84cd21dfe12ee1804e464aa
CodeShed <Bruce@bennettconsulting.eu>
2021-07-20 08:48:33 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test.FluentValidation.Validators.Models
+{
+    public class Product
+    {
+        public DateTime AvailableFrom { get; set; }
+        public DateTime? AvailableTo { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int ReorderAtItemsInStock { get; set; }
+        public decimal RRP { get; set; }
+    }
+}
---
+++ Validators/ProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentValidation;
+using Test.FluentValidation.Validators.Models;
+
+namespace Test.FluentValidation.Validators.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>, IValidator
+    {
+        public ProductValidator()
+        {
+            RuleFor(m => m.AvailableFrom).GreaterThanOrEqualTo(new DateTime(2000, 1, 1)).WithMessage("Available from must be greater than 1 Jan 2000");
+            RuleFor(m => m.AvailableTo)
+                .GreaterThanOrEqualTo(m => m.AvailableFrom)
+                .WithMessage("Available to must be greater than availble from")
+                .Unless(m => m.AvailableTo == null);
+            RuleFor(m => m.Name).NotEmpty()
+                    .Length(3, 5);
+            RuleFor(m => m.ProductId).GreaterThanOrEqualTo(0);
+            RuleFor(m => m.RRP).GreaterThan(0);
+        }
+    }
+}
